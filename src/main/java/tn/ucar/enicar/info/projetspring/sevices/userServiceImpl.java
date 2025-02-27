e5c247b8817e525b32fabfe9249f56f2f52fc66e
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-02-27 22:00:11 +0100

    fix: update userServiceImpl to use correct capitalization for User entity

--- src/main/java/tn/ucar/enicar/info/projetspring/sevices/userServiceImpl.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/sevices/userServiceImpl.java
@@ -2,7 +2,7 @@ package tn.ucar.enicar.info.projetspring.sevices;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import tn.ucar.enicar.info.projetspring.entities.user;
+import tn.ucar.enicar.info.projetspring.entities.User;
 import tn.ucar.enicar.info.projetspring.repositories.userRepo;
 
 import java.util.List;
@@ -11,7 +11,7 @@ import java.util.List;
 public class userServiceImpl implements userService {
     userRepo userRepo;
 
-    public List<user> retrieveAllUsers() {
+    public List<User> retrieveAllUsers() {
         return userRepo.findAll();
     }
 }
