51da4335a8f2131212845f3accd5f20253b2d7b9
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-02-27 21:58:23 +0100

    add AuthenticationService for user registration and authentication

---
+++ src/main/java/tn/ucar/enicar/info/projetspring/auth/AuthenticationService.java
@@ -0,0 +1,81 @@
+package tn.ucar.enicar.info.projetspring.auth;
+
+import lombok.RequiredArgsConstructor;
+import tn.ucar.enicar.info.projetspring.entities.Token;
+import tn.ucar.enicar.info.projetspring.entities.TokenType;
+import tn.ucar.enicar.info.projetspring.entities.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import tn.ucar.enicar.info.projetspring.config.JwtService;
+import tn.ucar.enicar.info.projetspring.entities.Role;
+import tn.ucar.enicar.info.projetspring.repositories.TokenRepository;
+import tn.ucar.enicar.info.projetspring.repositories.userRepo;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final userRepo repository ;
+    private final TokenRepository tokenRepository ;
+    private final PasswordEncoder passwordEncoder ;
+    private final JwtService jwtService ;
+    private final AuthenticationManager authenticationManager ;
+
+
+public AuthenticationResponse register (RegisterRequest request){
+   var user = User.builder()
+           .firstname(request.getFirstname())
+           .lastname(request.getLastname())
+           .email(request.getEmail())
+           .password(passwordEncoder.encode(request.getPassword()))
+           .role(Role.admin)
+           .build();
+   var savedUser =repository.save(user);
+   var jwtToken = jwtService.generateToken(user);
+   revokeAllUserTokens(user);
+    saveUserToken(savedUser, jwtToken);
+    return AuthenticationResponse.builder()
+            .token(jwtToken)
+            .build();
+}
+
+private void revokeAllUserTokens(User user){
+    var validUserTokens= tokenRepository.findAllValidTokensByUser(user.getId());
+            if(validUserTokens.isEmpty())
+                return;
+            validUserTokens.forEach(t ->{
+                t.setExpired(true);
+                t.setRevoked(true);
+            });
+            tokenRepository.saveAll(validUserTokens);
+}
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .revoked(false)
+                .expired(false)
+                .build();
+        tokenRepository.save(token);
+    }
+
+
+    public AuthenticationResponse authenticate (AuthenticationRequest request){
+    authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                    request.getEmail(),
+                    request.getPassword()));
+
+    var user = repository.findByEmail(request.getEmail())
+            .orElseThrow() ;
+    var jwtToken = jwtService.generateToken(user);
+    revokeAllUserTokens(user);
+    saveUserToken(user, jwtToken);
+    return AuthenticationResponse.builder()
+            .token(jwtToken)
+            .build();
+}
+}
