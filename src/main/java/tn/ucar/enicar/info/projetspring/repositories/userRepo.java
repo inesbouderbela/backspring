7ae76d14ae1fcdae00aaf9cf2d53f2a9951ab463
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-02-27 22:00:00 +0100

    fix: update userRepo to use correct capitalization for User entity and add findByEmail method

--- src/main/java/tn/ucar/enicar/info/projetspring/repositories/userRepo.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/repositories/userRepo.java
@@ -1,9 +1,11 @@
 package tn.ucar.enicar.info.projetspring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import tn.ucar.enicar.info.projetspring.entities.user;
+import tn.ucar.enicar.info.projetspring.entities.User;
 
-public interface userRepo extends JpaRepository<user, Integer> {
+import java.util.Optional;
 
+public interface userRepo extends JpaRepository<User, Integer> {
+Optional<User> findByEmail(String email);
 
 }
