d0a1c60308db40c546abfd069bc4bad4746d2c11
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-03-27 00:50:28 +0100

    add: implement CommandLineRunner to register an admin user on startup

--- src/main/java/tn/ucar/enicar/info/projetspring/ProjetspringApplication.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/ProjetspringApplication.java
@@ -1,7 +1,14 @@
 package tn.ucar.enicar.info.projetspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import tn.ucar.enicar.info.projetspring.auth.AuthenticationService;
+import tn.ucar.enicar.info.projetspring.auth.RegisterRequest;
+import tn.ucar.enicar.info.projetspring.entities.Role;
+
 
 @SpringBootApplication
 public class ProjetspringApplication {
@@ -10,4 +17,22 @@ public class ProjetspringApplication {
         SpringApplication.run(ProjetspringApplication.class, args);
     }
 
+
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(Role.ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+
+        };
+    }
 }
