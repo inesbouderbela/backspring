c325b63bd340a5e5b9de5ba50f48e622d512018d
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-03-27 00:50:22 +0100

    add: create Permission enum for role-based access control

---
+++ src/main/java/tn/ucar/enicar/info/projetspring/entities/Permission.java
@@ -0,0 +1,19 @@
+package tn.ucar.enicar.info.projetspring.entities;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Permission {
+    ADMIN_READ("admin:read"),
+    ADMIN_CREATE("admin:create"),
+    ADMIN_UPDATE("admin:update"),
+    ADMIN_DELETE("admin:delete"),
+    VOLUNTARY_READ("management:read"),
+    VOLUNTARY_CREATE("management:create"),
+    VOLUNTARY_UPDATE("management:update"),
+    VOLUNTARY_DELETE("management:delete");
+    private final String permission;
+
+}
