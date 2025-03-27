41e7a242e7458f849ce0b7499bd1ce8e6f904cb1
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-03-27 01:36:16 +0100

    add: implement createManager endpoint in AdminController with role-based access control

--- src/main/java/tn/ucar/enicar/info/projetspring/controllers/AdminController.java
+++ src/main/java/tn/ucar/enicar/info/projetspring/controllers/AdminController.java
@@ -1,12 +1,20 @@
 package tn.ucar.enicar.info.projetspring.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import tn.ucar.enicar.info.projetspring.auth.AuthenticationResponse;
+import tn.ucar.enicar.info.projetspring.auth.RegisterRequest;
+import tn.ucar.enicar.info.projetspring.sevices.AdminService;
 
 @RestController
 @RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor // Ajoutez cette annotation
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
+
+    private final AdminService adminService; // Gardez cette déclaration
 
     @GetMapping
     @PreAuthorize("hasAuthority('admin:read')")
@@ -19,14 +27,23 @@ public class AdminController {
     public String post() {
         return "POST:: admin controller";
     }
+
     @PutMapping
     @PreAuthorize("hasAuthority('admin:update')")
     public String put() {
         return "PUT:: admin controller";
     }
+
     @DeleteMapping
     @PreAuthorize("hasAuthority('admin:delete')")
     public String delete() {
         return "DELETE:: admin controller";
+    }
+
+    @PostMapping("/create-manager")
+    @PreAuthorize("hasAuthority('admin:create')")
+    public ResponseEntity<AuthenticationResponse> createManager(
+            @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(adminService.createManager(request));
     }
 }
